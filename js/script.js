08fe522e911450089d2f4aebd0e8dc22bb3067d1
ekaterina-chernykh <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-20 16:55:56 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Created by: Ekaterina
+// Created on: Oct 2022
+// This file contains the JS function for index.html
+
+"use strict"
+/**
+ * This function calculates XXX.
+ */
+function calculate() {
+  // input
+  const XXX = parseInt(document.getElementById("XXX").value)
+  const XXX = parseInt(document.getElementById("XXX").value)
+
+  // process
+  const XXX = XXX
+
+  // output
+  document.getElementById("XXX").innerHTML = "XXX" + "cm²"
+}
